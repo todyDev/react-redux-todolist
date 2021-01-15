0e437fb45ff50528bcb9038ed1bc8ca83a4ae238
Doyun <y0unn@hanmail.net>
2021-01-15 22:37:59 +0900

    :alembic: subscribe

--- src/index.js
+++ src/index.js
@@ -4,8 +4,10 @@ const add = document.getElementById("add");
 const minus = document.getElementById("minus");
 const number = document.querySelector("span");
 
+number.innerText = 0;
+
 const countModifier = (count = 0, action) => {
-  console.log(action);
+  console.log(count, action);
   if (action.type === "Add") {
     return count + 1;
   } else if (action.type === "Minus") {
@@ -16,33 +18,20 @@ const countModifier = (count = 0, action) => {
 };
 
 const countStore = createStore(countModifier);
-console.log(countStore.getState());
 
-countStore.dispatch({ type: "Add" });
-countStore.dispatch({ type: "Add" });
-countStore.dispatch({ type: "Add" });
-countStore.dispatch({ type: "Add" });
-countStore.dispatch({ type: "Add" });
-countStore.dispatch({ type: "Minus" });
-console.log(countStore.getState());
-
-/* let count = 0;
-
-number.innerText = count;
-
-const updateText = () => {
-  number.innerText = count;
+const onChange = () => {
+  number.innerText = countStore.getState();
 };
+
+countStore.subscribe(onChange);
 
 const handleAdd = () => {
-  count = count + 1;
-  updateText();
+  countStore.dispatch({ type: "Add" });
 };
+
 const handleMinus = () => {
-  count = count - 1;
-  updateText();
+  countStore.dispatch({ type: "Minus" });
 };
 
 add.addEventListener("click", handleAdd);
 minus.addEventListener("click", handleMinus);
- */
