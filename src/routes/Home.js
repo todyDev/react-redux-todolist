fc0fdb3543b982141c9edfa1585bef4135f6745f
Doyun <y0unn@hanmail.net>
2021-01-21 22:04:40 +0900

    :sparkles: create react app

---
+++ src/routes/Home.js
@@ -0,0 +1,24 @@
+import React, { useState } from "react";
+
+function Home() {
+  const [text, setText] = useState("");
+  function onChange(e) {
+    setText(e.target.value);
+  }
+  function onSubmit(e) {
+    e.preventDefault();
+    setText("");
+  }
+  return (
+    <>
+      <h1>할 일</h1>
+      <form onSubmit={onSubmit}>
+        <input type="text" value={text} onChange={onChange} />
+        <button>추가</button>
+      </form>
+      <ul></ul>
+    </>
+  );
+}
+
+export default Home;
