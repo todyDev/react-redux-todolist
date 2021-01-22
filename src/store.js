6c1cde468c6790b04c7d8c67312deedaa3b2b72e
Doyun <y0unn@hanmail.net>
2021-01-22 23:09:14 +0900

    :sparkles: create store

---
+++ src/store.js
@@ -0,0 +1,32 @@
+import { createStore } from "redux";
+
+const ADD_TODO = "ADD_TODO";
+const DEL_TODO = "DEL_TODO";
+
+// action router
+export const addToDo = (text) => {
+  return { type: ADD_TODO, id: Date.now(), text };
+};
+
+export const deleteToDo = (id) => {
+  return { type: DEL_TODO, id };
+};
+
+// reducer
+const reducer = (state = [], action) => {
+  switch (action.type) {
+    case ADD_TODO:
+      const newToDo = { text: action.text, id: action.id };
+      return [newToDo, ...state];
+    case DEL_TODO:
+      const cleanToDo = state.filter((toDo) => toDo.id !== action.id);
+      return cleanToDo;
+    default:
+      return state;
+  }
+};
+
+// store
+const store = createStore(reducer);
+
+export default store;
